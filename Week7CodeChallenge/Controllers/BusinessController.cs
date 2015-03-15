33366cd4e561559cc6229be5b016195b03b42816
PatrickSHYee <patrickshyee@gmail.com>
2015-03-15 11:33:43 -0600

    update

---
+++ Week7CodeChallenge/Controllers/BusinessController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Week7CodeChallenge.Controllers
+{
+    public class BusinessController : Controller
+    {
+        // GET: Business -> Work
+        public ActionResult Work()
+        {
+            return View();
+        }
+
+        // GET: Business -> Careers
+        public ActionResult Careers()
+        {
+            return View();
+        }
+    }
+}
